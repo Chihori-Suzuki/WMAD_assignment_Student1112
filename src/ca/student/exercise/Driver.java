b520dc59f12e8392aaaf57081503d27f5d9d7481
鈴木ちほり <suzukichihori@suzukichihoris-MacBook-Air.local>
2020-11-12 14:43:44 -0800

    second commit

--- src/ca/student/exercise/Driver.java
+++ src/ca/student/exercise/Driver.java
@@ -1,5 +1,6 @@
 package ca.student.exercise;
 
+import java.util.InputMismatchException;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Scanner;
@@ -28,31 +29,37 @@ public class Driver {
 		Student s1 = new Student(100, "email1@mail.com", "Aya Aikawa");
 		Student s2 = new Student(200, "email2@mail.com", "Ami Yamakawa");
 		Student s3 = new Student(300, "email3@mail.com", "Yuri Shido");
-		
-		System.out.println("Write your name, please.");
-		int id = input.nextInt();
-		System.out.println("Write your email, please");
-		String email = input.nextLine();
-		System.out.println("Write your name, please");
-		String name = input.nextLine();
 
-		Student s4 = new Student(id, email, name);
-		
-		
 		LinkedList<Student> list = new LinkedList<Student>();
 		list.add(s1);
 		list.add(s2);
 		list.add(s3);
-		list.add(s4);
 		
-		printIterable(list.iterator());
-		
-		System.out.println("Write the ID what you remove.");
+		try {
+			System.out.println("Write your id, please.");
+			int id = input.nextInt();
+			System.out.println("Write your email, please");
+			String email = input.nextLine();
+			System.out.println("Write your name, please");
+			String name = input.nextLine();
 
-		int delId = input.nextInt();
+			Student s4 = new Student(id, email, name);
+
+			list.add(s4);
+			
+			printIterable(list.iterator());
+			
+			System.out.println("Write the ID what you remove.");
+			int delId = input.nextInt();
+
+			removeIterable(list.iterator(), delId);
+			System.out.println(list);
+			
+		} catch (InputMismatchException ex) {
+			System.err.println("your input is incorrect. write again please.");
+		} 
 		
-		removeIterable(list.iterator(), delId);
-		System.out.println(list);
+		
 		
 	}
 
